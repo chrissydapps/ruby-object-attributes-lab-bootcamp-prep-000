3ab5bacffc6df86d9599b82f0f762c525e7d849b
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-20 16:11:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,15 +7,15 @@ class Dog
   def name
     @name
   end
-  
+
   def name= (new_name)
     @name = new_name
   end
-  
+
   def breed
     @breed
   end
-  
+
   def breed= (dog_breed)
     @breed = dog_breed
   end
