22ecf4bcf15a6b4eeac6b008b70e8c4252502ff9
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-20 16:20:23 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,8 @@
 class Person
-  def initialize(name, job)
-    @name = name
-    @job = job
-  end
+  # def initialize(name, job)
+  #   @name = name
+  #   @job = job
+  # end
 
   def name
     @name
